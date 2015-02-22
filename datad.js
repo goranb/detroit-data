aebd470f15e0b8f8715dd7414773a3bcd91ce6c7
goranb <git@gbut.com>
2015-02-22 01:29:45 -0500

    clusters

--- datad.js
+++ datad.js
@@ -4,39 +4,54 @@ var Data;
 
 window.onload = function(e)
 {
-	Data = new function()
-	{
-		console.log('?');
-		var worker = new Worker('worker.js');
+    Data = new function()
+    {
+        var worker = new Worker('worker.js');
 
-		var map = L.map('map', {
-	    	center: [42.331389, -83.045833],
-	    	zoom: 12
-		});
+        var map = L.map('map', {
+            center: [42.331389, -83.045833],
+            zoom: 12
+        });
 
-		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-		}).addTo(map);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-		worker.onmessage = function(e)
-		{
-			var data = e.data;
+        var markers = new L.MarkerClusterGroup({
+            removeOutsideVisibleBounds: true,
+            iconCreateFunction: function(cluster)
+            {
+                var level = Math.floor(cluster.getChildCount() / 10);
+                if (level > 10)
+                {
+                    level = 10;
+                }
+                return new L.DivIcon({
+                    className: 'icon cluster-icon cluster-icon-' + level,
+                    html: cluster.getChildCount()
+                });
+            }
+        });
 
-			for (var i in data)
-			{
-				L.marker([data[i].location.latitude, data[i].location.longitude], {
-						riseOnHover: true
-					})
-					.addTo(map)
-	    			.bindPopup(data[i].property_address + '<br>' + data[i].name);
-			}
-		}
+        worker.onmessage = function(e)
+        {
+            var data = e.data;
 
-		worker.postMessage("https://data.detroitmi.gov/resource/3t7p-zq9t.json");
-	};
+            for (var i in data)
+            {
+                var m = L.marker([data[i].location.latitude, data[i].location.longitude], {
+                        icon: new L.DivIcon({
+                            className: 'icon'
+                        })
 
-	document.getElementById('dataset-toggle').onclick = function()
-	{
-		document.getElementById('dataset-data').style.display = 'block';
-	}
+                    })
+                    .bindPopup(data[i].property_address + '<br>' + data[i].name);
+
+                markers.addLayer(m);
+            }
+            map.addLayer(markers);
+        }
+
+        worker.postMessage("https://data.detroitmi.gov/resource/3t7p-zq9t.json");
+    };
 };
