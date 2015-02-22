859a640ba7abbe987887146915b38233c6de4004
goranb <git@gbut.com>
2015-02-21 22:09:15 -0500

    window.onload

--- datad.js
+++ datad.js
@@ -1,33 +1,37 @@
 "use strict";
 
-var Data = new function()
+var Data;
+
+window.onload = function(e)
 {
-	var worker = new Worker('worker.js');
-
-	var map = L.map('map', {
-    	center: [42.331389, -83.045833],
-    	zoom: 12
-	});
-
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
-
-	worker.onmessage = function(e)
+	Data = new function()
 	{
-		var data = e.data;
+		console.log('?');
+		var worker = new Worker('worker.js');
 
-		for (var i in data)
+		var map = L.map('map', {
+	    	center: [42.331389, -83.045833],
+	    	zoom: 12
+		});
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		worker.onmessage = function(e)
 		{
-			L.marker([data[i].location.latitude, data[i].location.longitude], {
-					riseOnHover: true
-				})
-				.addTo(map)
-    			.bindPopup(data[i].property_address + '<br>' + data[i].name);
+			var data = e.data;
+
+			for (var i in data)
+			{
+				L.marker([data[i].location.latitude, data[i].location.longitude], {
+						riseOnHover: true
+					})
+					.addTo(map)
+	    			.bindPopup(data[i].property_address + '<br>' + data[i].name);
+			}
 		}
 
-		//document.body.innerHTML = e.data;
-	}
-
-	worker.postMessage("https://data.detroitmi.gov/resource/3t7p-zq9t.json");
+		worker.postMessage("https://data.detroitmi.gov/resource/3t7p-zq9t.json");
+	};
 };
