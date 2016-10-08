2a43791592d5cbe6ea37626f5fac90236e93396c
Goran Butorac <git@gbut.com>
2016-10-08 00:44:06 +0000

    remove leaflet cluster

--- ddata.js
+++ ddata.js
@@ -9,7 +9,7 @@ window.onload = function(e)
         var worker = new Worker('worker.js');
 
         var map = L.map('map', {
-            center: [42.331389, -83.045833],
+            center: [42.361389, -83.045833],
             zoom: 12
         });
         
@@ -17,36 +17,12 @@ window.onload = function(e)
             attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
         }).addTo(map);
 
-        var markers = new L.MarkerClusterGroup({
-            removeOutsideVisibleBounds: true,
-            iconCreateFunction: function(cluster)
-            {
-                var level = Math.floor(cluster.getChildCount() / 10);
-                if (level > 10)
-                {
-                    level = 10;
-                }
-                return new L.DivIcon({
-                    className: 'icon cluster-icon cluster-icon-' + level,
-                    html: '<span class="label">' + cluster.getChildCount() + '</span>',
-                    iconSize: [50, 50],
-                    iconAnchor: [25, 25],
-                    popupAnchor: [25, 25]
-                });
-            }
-        });
-
         worker.onmessage = function(e)
         {
             var data = e.data;
 
             for (var d of data)
             {
-                if (!d.latitude || !d.longitude){
-                    console.log(d.longitude);
-                    console.log(d.latitude);
-                    continue;
-                }
                 var m = L.marker([d.latitude, d.longitude], {
                         icon: new L.DivIcon({
                             className: 'icon',
@@ -54,12 +30,10 @@ window.onload = function(e)
                             popupAnchor: [5, 5]
                         })
                     })
-                    //.addTo(map)
+                    .addTo(map)
                     .bindPopup(d.address+ '<br>' + d.contractor_name + '<br>$' + parseFloat(d.price).toFixed(2));
 
-                markers.addLayer(m);
             }
-            map.addLayer(markers);
         }
 
         worker.postMessage("https://data.detroitmi.gov/resource/nfx3-ihbp.json");
